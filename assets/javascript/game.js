9f2c2b82a83ea4341e739efda5c5cf7c2c97a11b
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-10-16 18:21:53 -0500

    DRYs up my code a little bit using indexOf

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,23 +6,18 @@ var tries = 15;
 var alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S",
 			"T","U","V","W","X","Y","Z"];
 
-var answer = {	pacman: ["P","A","C","M","A","N"],
-				tetris: ["T","E","T","R","I","S"] 
-		
-	};
+var pacman = ["P","A","C","M","A","N"];
 
-	
-	
-	document.onkeyup = function(event) {
+	console.log(pacman);
+		document.onkeyup = function(event) {
 
 
 		var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
 
-		if(userGuess !== answer.pacman[0] && userGuess !== answer.pacman[1] &&
-		userGuess !== answer.pacman[2] && userGuess !== answer.pacman[3]
-			&& userGuess !== answer.pacman[4] && userGuess !== answer.pacman[5]) {
+		if(pacman.indexOf(userGuess) === -1) {
 				tries--;
 			document.getElementById("tries").innerHTML = tries;
+
 			}
 
 		if(tries === 0) {
@@ -31,34 +26,32 @@ var answer = {	pacman: ["P","A","C","M","A","N"],
 
 		}
 
-
-
-		if(userGuess === answer.pacman[0]) {
+		if(pacman.indexOf(userGuess) === 0) {
 		document.getElementById("l0").innerHTML = userGuess;
 		correctGuesses++;
 		}  
-		if(userGuess === answer.pacman[1]) {
+		if(pacman.indexOf(userGuess) === 1) {
 		document.getElementById("l1").innerHTML = userGuess;
 		correctGuesses++;
 		}
-		if(userGuess === answer.pacman[2]) {
+		if(pacman.indexOf(userGuess) === 2) {
 		document.getElementById("l2").innerHTML = userGuess;
 		correctGuesses++;
 		}
-		if(userGuess === answer.pacman[3]) {
+		if(pacman.indexOf(userGuess) === 3) {
 		document.getElementById("l3").innerHTML = userGuess;
 		correctGuesses++;
 		}
-		if(userGuess === answer.pacman[4]) {
+		if(pacman.indexOf(userGuess,3) === 4) {
 		document.getElementById("l4").innerHTML = userGuess;
 		correctGuesses++;
 		}
-		if(userGuess === answer.pacman[5]) {
+		if(pacman.indexOf(userGuess) === 5) {
 		document.getElementById("l5").innerHTML = userGuess;
 		correctGuesses++;
 		}
 
-		if(correctGuesses === answer.pacman.length) {
+		if(correctGuesses === pacman.length) {
 
 			document.getElementById("solved").innerHTML = "You Win!";
 			wins++;
