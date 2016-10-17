83790169a87f333f084b6b87ec851a62af75107c
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-10-17 12:13:40 -0500

    Adds more function to my function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,48 +2,45 @@
 var pacman = ['P','A','C','M','A','N'];
 var digdug = ['D','I','G','D','U','G'];
 var mario = ['M','A','R','I','O'];
+var tetris = ['T','E','T','R','I','S'];
 var wins = 0;
+var tries = 10;
 var spentLetters = [];
 
 document.onkeyup = function(event) {
 
 
 		var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
-
+// ******* My Phat Phunction ***********************************
 		function check(arr) {
+			if(arr.length === 0) {
+			console.log('you win');
 
-			for(var i = 0; i < arr.length; i++) {
+			} else {
+
+				for(var i = 0; i < arr.length; i++) {
 
 				if(userGuess === arr[i]) {
 					console.log(userGuess);
 					arr.splice(arr.indexOf(userGuess),1);
 					console.log(arr);
-					if(arr.length === 0) {
-						console.log("done");
-						wins++;
-						
-					 
+					} else {
+						if(spentLetters.indexOf(userGuess) === -1) {
+						spentLetters.push(userGuess);
+						console.log(spentLetters.toString())
+						tries--;
 					}
+				}//nested if
+			}//else
+		}//for loop
+	} // my function
+if(tries === 0) {
 
-				}
+		console.log("game over");
 
-			}
-		}
-	check(mario);
-	
-		if(mario.length === 0) {
-		check(pacman);
 	}
-	console.log(wins);
-}
 
-	
+	check(tetris); 
 
 
-			// if(spentLetters.indexOf(userGuess) === -1 && tries > 0) {
-			// spentLetters.push(userGuess);
-			// console.log(spentLetters.toString());
-				// tries--;
-			// console.log(tries);
-			// }
-
+ } // keycode event
