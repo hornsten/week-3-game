7136a4fd30d1f701eb95248e592fee75f4f5fd8d
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-10-17 23:38:34 -0500

    commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,47 +2,45 @@ var pacman = "PACMAN";
 var digdug = ['D','I','G','D','U','G'];
 var mario = ['M','A','R','I','O'];
 var tetris = ['T','E','T','R','I','S'];
+var centipede = "CENTIPEDE";
 var wins = 0;
 var tries = 10;
 var spentLetters = [];
 
-// ******* My Functions ***********************************
 
 		function check(arr) {
 
 		document.onkeyup = function(event) {
 
-
+// adds correct letters to span elements in the DOM
 		var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
 				
 				for(var i = 0; i < arr.length; i++) {
 
 				if(userGuess === arr[i]) {
 					
-					document.getElementsByTagName("LI")[i].innerHTML = userGuess;
-					} 
-				
-		}//for loop
+					document.getElementsByTagName("SPAN")[i].innerHTML = userGuess;
 
+				} 
+
+		}
+
+// Handles the incorrect guesses
 		if(spentLetters.indexOf(userGuess) === -1 && arr.indexOf(userGuess) === -1){
 					spentLetters.push(userGuess);
 					document.getElementById("used").innerHTML = spentLetters.toString();
 					tries--;
 					document.getElementById("tries").innerHTML = tries;
 					}
-		if(arr.length === 0 && tries > 0) {
-			wins++;
-			document.getElementById("wins").innerHTML = wins;
-			document.getElementById("solved").style.color="green";
-			document.getElementById("solved").innerHTML = "you win!";
-			} 
-
-			if (arr.length > 0 && tries === 0) {
+//if you use up all your tries, you lose.					
+		if (tries === 0) {
 			document.getElementById("solved").innerHTML = "you lose";
 			}
+			
+		} 
 
-		}
+	}
 		
-	} // my function
 
-check(digdug);
+
+check(tetris);
