d17f1a3e293e82ddac6dff0ffff1b0892f1f3fa3
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-10-19 09:13:19 -0500

    Adds a button to start the game.  Adds jQuery CDN.  Replaces 'word' with 'arr' throughout the game to make one big function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,44 @@
 var words = ['PACMAN','DIGDUG','SUPERMARIOBROS','TETRIS','CENTIPEDE','SPACEINVADERS','GALAGA','ASTEROIDS','DONKEYKONG','PONG','TRON','BURGERTIME','BERZERK','QBERT'];
-var word = words[Math.floor(Math.random() * words.length)];
+
 var wins = 0;
 var tries = 10;
 var spentLetters = [];
 var correctGuess = 0;
+var gameStarted = false;
+// if the game hasn't started, load the big function with pressing key.  if it HAS started, load the blanks.
+
 
 // Creates span elements in the DOM equal to the word's length
-function getBlanks(arr) {
+
+$(document).ready(function(){
+
+	 $("#start").on("click", function(){
+        var word = words[Math.floor(Math.random() * words.length)];
+	 	getBlanks(word);
+    })
+
+ function getBlanks(arr) {
 
 	var htmlElements = "";
-	for (var i = 0; i < word.length; i++) {
+	for (var i = 0; i < arr.length; i++) {
    	htmlElements += '<span>_</span>';
 	}
 	var blanks = document.getElementById("word");
 	blanks.innerHTML = htmlElements;
 
-}
 
 
-// When key is pressed, it triggers the getBlanks function
-window.addEventListener("keypress", checkKeyPressed, false);
- 
-function checkKeyPressed(e) {
-    if (e.charCode == "32") {
-   	getBlanks(word);
-    }
-}
 
-document.onkeyup = function startGame(event) {
+
+
+document.onkeyup = function(event) { 
 
 // adds correct letters to span elements in the DOM
 var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
 				
-				for(var i = 0; i < word.length; i++) {
+				for(var i = 0; i < arr.length; i++) {
 
-				if(userGuess === word[i]) {
+				if(userGuess === arr[i]) {
 					
 
 					document.getElementsByTagName("SPAN")[i].innerHTML = userGuess;
@@ -43,7 +47,7 @@ var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
 				}
   	}	
 // Determines when you have won
-if(correctGuess === word.length) {
+if(correctGuess === arr.length) {
   	wins++;
   	document.getElementById("solved").style.color = "green";
   	document.getElementById("solved").innerHTML = "You Win!";
@@ -53,7 +57,7 @@ if(correctGuess === word.length) {
 
 
 // Handles the incorrect guesses
-		if(spentLetters.indexOf(userGuess) === -1 && word.indexOf(userGuess) === -1){
+		if(spentLetters.indexOf(userGuess) === -1 && arr.indexOf(userGuess) === -1){
 					spentLetters.push(userGuess);
 					document.getElementById("used").innerHTML = spentLetters.toString();
 					tries--;
@@ -64,9 +68,7 @@ if(correctGuess === word.length) {
 			document.getElementById("solved").innerHTML = "you lose";
 		
 			}
-			
-		} 
-
-	
-
-
+			 
+}
+}
+})
