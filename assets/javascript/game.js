3d9ccfc5fca9eafbaf5ceedb5c0641a8a9d74cde
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-10-19 21:46:01 -0500

    Adding pictures and audio files to game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,11 @@
-var words = ['pacman','digdug','supermariobros','tetris','centipede','spaceinvaders','galaga','asteroids','donkeykong','pong','tron','burgertime','berzerk','qbert'];
+var words = [
+  { url: 'https://pixabay.com/static/uploads/photo/2013/07/12/14/58/indonesian-149128_960_720.png', text: 'pacman', sound: 'pacman.mp3' },
+  { url: 'https://pixabay.com/static/uploads/photo/2013/07/12/12/57/ghost-146620_960_720.png', text: 'mario', sound: 'mario.mp3'},
+  { url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR9hsBA2tKTpSidZZcBoW-vUkUekatTndWWEJ3sozU5Pz47G8gF', text: 'digdug', sound: 'digdug.mp3'}
+];
+
+
+//'digdug','supermariobros','tetris','centipede','spaceinvaders','galaga','asteroids','donkeykong','pong','tron','burgertime','berzerk','qbert'];
 
 var wins = 0;
 var tries = 10;
@@ -6,7 +13,6 @@ var spentLetters = [];
 var usedCorrect = [];
 var correctGuess = 0;
 var gameStarted = false;
-// if the game hasn't started, load the big function with pressing key.  if it HAS started, load the blanks.
 
 
 // Creates span elements in the DOM equal to the word's length
@@ -15,7 +21,8 @@ $(document).ready(function(){
 
 $("#start").on("click", function(){
         var word = words[Math.floor(Math.random() * words.length)];
-	 	console.log(word);
+	 	clue = word.text;
+	 	console.log(clue);
 	 	spentLetters = [];
 	 	correctGuess = 0;
 	 	tries = 10;
@@ -27,7 +34,7 @@ $("#start").on("click", function(){
 
 
 	var htmlElements = "";
-	for (var i = 0; i < word.length; i++) {
+	for (var i = 0; i < clue.length; i++) {
    	htmlElements += '<span>_</span>';
 	}
 	var blanks = document.getElementById("word");
@@ -39,9 +46,9 @@ document.onkeyup = function(event) {
 // adds correct letters to span elements in the DOM
 var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 				
-				for(var i = 0; i < word.length; i++) {
+				for(var i = 0; i < clue.length; i++) {
 
-				if(userGuess === word[i]) {
+				if(userGuess === clue[i]) {
 					document.getElementsByTagName("SPAN")[i].innerHTML = userGuess;
 
 					var x = document.getElementById("word");
@@ -55,18 +62,19 @@ var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 // Determines when you have won
 var filledSpans = document.getElementsByClassName("filled");
 
-if(filledSpans.length === word.length) {
+if(filledSpans.length === clue.length) {
   	document.onkeyup = null;
   	document.getElementById("solved").style.color = "green";
   	document.getElementById("solved").innerHTML = "You Win!";
   	wins++;
   	document.getElementById("wins").innerHTML = "Wins: " + wins;
- 
+ 	document.getElementById("myAudio").src = word.sound;
+ 	document.getElementById("myImg").src = word.url;
   
 }
 
 // Handles the incorrect guesses
-		if(spentLetters.indexOf(userGuess) === -1 && word.indexOf(userGuess) === -1){
+		if(spentLetters.indexOf(userGuess) === -1 && clue.indexOf(userGuess) === -1){
 					spentLetters.push(userGuess);
 					document.getElementById("used").innerHTML = "Incorrect Guesses: " + spentLetters.toString();
 					tries--;
